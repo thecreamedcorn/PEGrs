8cd2feb9446f36935d1eb95deffb7b74821cedb5
thecreamedcorn <wgaboury@gmail.com>
2018-03-10 23:29:05 -0500

    wrote module

--- src/lib/peg_rs/input/parsable.rs
+++ src/lib/peg_rs/input/parsable.rs
@@ -1,47 +1,49 @@
-use std::str::Chars;
+use std::str::CharIndices;
 
-pub trait Parsable {
-    type Loc: Clone;
-
-    fn peek(&self) -> Option<char>;
-    fn next(&mut self) -> Option<char>;
-    fn get_loc(&self) -> Self::Loc;
-    fn goto_loc(&mut self, loc: &Self::Loc);
-}
-
-pub struct ParsableString<'a> {
+pub struct Parsable<'a> {
     string: &'a str,
-    loc: Chars<'a>,
-    cur: Option<char>
+    loc: CharIndices<'a>,
+    cur: Option<(usize, char)>
 }
 
-impl<'a> ParsableString<'a> {
-    pub fn new(string: &'a str) -> ParsableString<'a> {
-        let mut loc = string.chars();
+impl<'a> Parsable<'a> {
+    pub fn new(string: &'a str) -> Parsable {
+        let mut loc = string.char_indices();
         let cur = loc.next();
-
-        ParsableString { string, loc, cur }
-    }
-}
-
-impl<'a> Parsable for ParsableString<'a> {
-    type Loc = Chars<'a>;
-
-    fn peek(&self) -> Option<char> {
-        self.cur
+        Parsable { string, loc, cur }
     }
 
-    fn next(&mut self) -> Option<char> {
+    pub fn peek(&self) -> Option<char> {
+        match self.cur {
+            Option::Some((_, c)) => Option::Some(c),
+            Option::None => Option::None
+        }
+    }
+
+    pub fn next(&mut self) -> Option<char> {
         self.cur = self.loc.next();
         self.peek()
     }
 
-    fn get_loc(&self) -> Self::Loc {
-        self.loc.clone()
+    pub fn get_loc(&self) -> usize {
+        match self.cur {
+            Option::Some((s, _)) => s,
+            Option::None => self.string.len()
+        }
     }
 
-    fn goto_loc(&mut self, loc: &Self::Loc) {
-        self.loc = loc.clone();
+    pub fn goto_loc(&mut self, loc: usize) {
+        if loc >= self.string.len() {
+            self.loc = self.string.char_indices();
+            self.cur = Option::None
+        } else {
+            self.loc = self.string[loc..].char_indices();
+            self.cur = self.loc.next()
+        }
+    }
+
+    pub fn sub(&self, loc1: usize, loc2: usize) -> &str {
+        &self.string[loc1..loc2]
     }
 }
 
@@ -50,14 +52,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parsable_string() {
-        let mut input = ParsableString::new("test");
+    fn test_parsable() {
+        let mut input = Parsable::new("test");
         let loc = input.get_loc();
         assert_eq!(input.peek().unwrap(), 't');
         input.next();
         assert_eq!(input.next().unwrap(), 's');
-        input.goto_loc(&loc);
+        input.goto_loc(loc);
         assert_eq!(input.next().unwrap(), 'e');
-        input.goto_loc(&loc);
+        input.goto_loc(loc);
+        assert_eq!(input.sub(0, 4), "test");
     }
 }
